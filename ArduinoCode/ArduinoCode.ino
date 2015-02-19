11936ca50e92ea032352427744bb5082edc7dde3
Ole-Magnus Pedersen <pedersen.olemagnus@gmail.com>
2015-02-19 14:45:57 +0100

    Sideview

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,12 +1,15 @@
+#include <QTRSensors.h>
+#include <PLab_ZumoMotors.h>
+#include <ZumoReflectanceSensorArray.h>
+#include <ZumoBuzzer.h>
+#include <Pushbutton.h>
+#include <ZumoMotors.h>
+#include <NewPing.h>
 /*
 Drive forward and turn left or right when border is detected
 - Only reflectionsensor 0 and 5 are used.
 */
-#include <ZumoMotors.h>
-#include <Pushbutton.h>
-#include <QTRSensors.h>
-#include <NewPing.h>
-#include <ZumoReflectanceSensorArray.h>
+
 
 #define LED 13
 
@@ -14,57 +17,80 @@ Drive forward and turn left or right when border is detected
 #define QTR_THRESHOLD 1600 //
 
 // these might need to be tuned for different motor types
-#define REVERSE_SPEED 100 // 0 is stopped, 400 is full speed
-#define TURN_SPEED 100
-#define FORWARD_SPEED 100
+#define REVERSE_SPEED 200 // 0 is stopped, 400 is full speed
+#define TURN_SPEED 200
+#define FORWARD_SPEED 200
 #define FAST_FORWARD_SPEED 400
-#define REVERSE_DURATION 200 // ms
+#define REVERSE_LENGTH 10
 #define TURN_DURATION 300 // ms
-#define TRIG_PIN 1
-#define ECHO_PIN 3
+#define SERVO_PIN 11
+#define PING_PIN_1 1
+#define PING_PIN_2 3
+#define PING_PIN_3 4
 #define MAX_DIST 20
 
-ZumoMotors motors;
+PLab_ZumoMotors motors;
+ZumoMotors motors1;
 Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
 
-NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DIST);
+NewPing sonar(PING_PIN_1, PING_PIN_1, MAX_DIST), sonarR(PING_PIN_2, PING_PIN_2, MAX_DIST), sonarL(PING_PIN_3, PING_PIN_3, MAX_DIST);
 
 #define NUM_SENSORS 6
+
+//unsigned long servoTime;
 unsigned int sensor_values[NUM_SENSORS];
+//int servoState;   
 ZumoReflectanceSensorArray sensors;
 
+//Servo servo;
 
 void setup()
 {
+  //servo.attach(SERVO_PIN);
   sensors.init();
+  //servoTime = millis();
+  //servoState = 2;
   //button.waitForButton();
 }
 void loop() {
   sensors.read(sensor_values);
   int time = sonar.ping();
   float dist = sonar.convert_cm(time);
+  float distR = sonarR.ping_cm();
+  float distL = sonarL.ping_cm();
   
   if (sensor_values[0] > QTR_THRESHOLD) {
     // if leftmost sensor detects line, reverse and turn to the right
-    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
-    delay(REVERSE_DURATION);
-    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-    delay(TURN_DURATION);
-    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    motors.backward(REVERSE_SPEED, REVERSE_LENGTH);
+    motors.turnRight(TURN_SPEED, 135);
   }
   else if (sensor_values[5] > QTR_THRESHOLD) {
     // if rightmost sensor detects line, reverse and turn to the left
-    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
-    delay(REVERSE_DURATION);
-    motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-    delay(TURN_DURATION);
-    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    motors.backward(REVERSE_SPEED, REVERSE_LENGTH);
+    motors.turnLeft(TURN_SPEED, 135);
   }
   else if(dist != 0){
-    motors.setSpeeds(FAST_FORWARD_SPEED, FAST_FORWARD_SPEED);
+    motors1.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+  else if(distR != 0){
+    motors.turnRight(TURN_SPEED, 90);
+  }
+  else if(distL != 0){
+    motors.turnLeft(TURN_SPEED, 90); 
   }
   else {
-    motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+    motors1.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
   }
+//  if(servoTime + 50000 < millis()){
+//      if(servoState == 1){
+//        servo.write(90);
+//        servoState = 2;
+//      }
+//      else{
+//       servo.write(170);
+//       servoState = 1;
+//      }
+//      servoTime = millis();
+//    }
 }
 
